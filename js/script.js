29182c5cfded9eb901ff5277972ea4d6cb1bfc9f
River Merrill <rivermerrill.me@gmail.com>
2016-12-03 23:44:12 -0700

    Added product section and animations

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ app.controller('MainController', function ($scope, $http) {
   var rand = Math.floor(Math.random() * 30) + 20;
   $scope.subSize = rand;
   rand = Math.floor(Math.random() * subArr.length - 1) + 1;
-  console.log(rand);
   $scope.subMessage = subArr[rand];
   $http.get('https://api.github.com/repos/rivermerrill/merrills/events').then(function (res, err) {
     $scope.mostRecent = res.data[0].payload.commits[0].message;
@@ -33,4 +32,16 @@ var controller = new ScrollMagic.Controller();
 var scene1 = new ScrollMagic.Scene({ triggerElement: "#subliminal" })
 // trigger a velocity opaticy animation
 .setVelocity("#subliminal", { translateY: -25, opacity: 100 }, { duration: 600 })
+.addTo(controller);
+
+var scene2 = new ScrollMagic.Scene({triggerElement: ".leftanim"})
+.setVelocity(".leftAnim", {translateX: +35, opacity: 100}, {duration: 1000})
+.addTo(controller);
+
+var scene3 = new ScrollMagic.Scene({triggerElement: ".rightAnim"})
+.setVelocity(".rightAnim", {translateX: -35, opacity: 100}, {duration: 1000})
+.addTo(controller);
+
+var scene3 = new ScrollMagic.Scene({triggerElement: ".products"})
+.setVelocity(".products", {opacity: 100}, {duration: 1000})
 .addTo(controller);

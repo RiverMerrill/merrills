919286c2b872dd7e80a6fde7c1cfb9d2d41984d5
River Merrill <rivermerrill.me@gmail.com>
2016-12-07 01:39:39 -0700

    WARNING: You must use '/#/' to access home page

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var port = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, '/')))
 app.get('/', function(res, res){
   res.sendFile(path.join(__dirname + '/index.html'));
-})
+}) 
 app.listen(port, function(){
     console.log('app is listening on port: ' + port)
 })
